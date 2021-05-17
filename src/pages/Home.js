4db13eb2d6469be4ca0005a27635e6ad8c5c6458
monsterkosh <mnnnkosh@gmail.com>
2021-05-17 20:29:45 -0300

    color change

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,31 +1,33 @@
 import React from 'react';
-import '../styles.css';
+import '../styles/HomeStyles.css';
 import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
-    <div className='welcome-msg'>
-      <div className='mainTitle'>
-        <h1>Front End Libraries Certification</h1>
-      </div>
-      <div className='list'>
-        <ul>
-          <p>
-            <Link to='/quotemachine'>Random Quote Machine</Link>
-          </p>
-          <p>
-            <Link to='/'>Markdown Previewer</Link>
-          </p>
-          <p>
-            <Link to='/'>Drum Machine</Link>
-          </p>
-          <p>
-            <Link to='/'>JavaScript Calculator</Link>
-          </p>
-          <p>
-            <Link to='/'>25 + 5 Clock</Link>
-          </p>
-        </ul>
+    <div className='container'>
+      <div className='welcome-msg'>
+        <div className='mainTitle'>
+          <h1>Front End Libraries Certification</h1>
+        </div>
+        <div className='list'>
+          <ul>
+            <p>
+              <Link to='/quotemachine'>Random Quote Machine</Link>
+            </p>
+            <p>
+              <Link to='/'>Markdown Previewer</Link>
+            </p>
+            <p>
+              <Link to='/'>Drum Machine</Link>
+            </p>
+            <p>
+              <Link to='/'>JavaScript Calculator</Link>
+            </p>
+            <p>
+              <Link to='/'>25 + 5 Clock</Link>
+            </p>
+          </ul>
+        </div>
       </div>
     </div>
   );
--- src/pages/QuoteMachine.js
+++ src/pages/QuoteMachine.js
@@ -1,48 +1,88 @@
+// @ts-nocheck
 import React, { useState, useEffect } from 'react';
 import '../styles/QMStyles.css';
-import { FaTwitter, FaFacebook } from 'react-icons/fa';
-import QuoteBox from '../components/QuoteBox';
+import { FaTwitter } from 'react-icons/fa';
+import COLORS_ARRAY from '../components/ColorsArray';
 
 const QuoteMachine = () => {
   const quoteDB =
     'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json';
 
   const defaultQuote = {
-    quote: '',
-    author: '',
+    quote: 'Winning isn’t everything, but wanting to win is.',
+    author: 'Vince Lombardi',
   };
 
   const [quotesArray, setQuotesArray] = useState();
   const [randomQuote, setRandomQuote] = useState(defaultQuote);
+  const [randomColor, setRandomColor] = useState('#333');
 
-  const randomNumber = (array) => {
+  async function fetchDB(quoteDB) {
+    let response = await fetch(quoteDB);
+    response = await response.json();
+    setQuotesArray(response.quotes);
+  }
+
+  // console.log(quotesArray);
+
+  const getRandomNumber = (array) => {
     let randomNum = Math.floor(Math.random() * array.length);
     return randomNum;
   };
 
-  async function fetchDB(quoteDB) {
-    fetch(quoteDB)
-      .then((response) => response.json())
-      .then((data) => setQuotesArray(data));
-  }
+  const handleNewQuote = (array) => {
+    let randomQuoteNumber = getRandomNumber(array);
+    let randomColorNumber = getRandomNumber(COLORS_ARRAY);
+    console.log(randomQuoteNumber);
+    setRandomQuote(array[randomQuoteNumber]);
+    setRandomColor(COLORS_ARRAY[randomColorNumber]);
 
-  const handleNewQuote = (quotesArray) => {
-    let randomQuoteNumber = randomNumber(quotesArray.quotes);
-    setRandomQuote(quotesArray.quotes[randomQuoteNumber]);
     console.log(randomQuote);
   };
 
   useEffect(() => {
     fetchDB(quoteDB);
-  }, []);
+  }, [quoteDB]);
 
   return (
-    <div>
-      {randomQuote.quote === '' ? (
-        <div>NO</div>
-      ) : (
-        <QuoteBox randomQuote={randomQuote} handleNewQuote={handleNewQuote} />
-      )}
+    <div className='containerMachine'>
+      <div
+        className='quote-machine'
+        style={{ backgroundColor: randomColor, color: 'black' }}
+      >
+        <div className='quoteBox' id='quote-box'>
+          <div className='inside-box'>
+            <div className='text' id='text'>
+              <span className='double-quotes'>" </span>
+              {randomQuote.quote}
+              <span className='double-quotes'> "</span>
+            </div>
+            <div className='author' id='author'>
+              - {randomQuote.author}
+            </div>
+            <div className='buttons'>
+              <a
+                href={`https://twitter.com/intent/tweet?text=${randomQuote.quote}-${randomQuote.author}&hashtags=quote`}
+                id='tweet-quote'
+                className='socials'
+                style={{ color: randomColor }}
+              >
+                <FaTwitter size={20} />
+              </a>
+
+              <div className='new-quote' id='new-quote'>
+                <button
+                  className='new-quote-button'
+                  onClick={() => handleNewQuote(quotesArray)}
+                >
+                  New quote
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className='creator'>Created by Emiliano Kosh</div>
+      </div>
     </div>
   );
 };
